2fa32e21e9afa3a52dbfdf79edba3e9959ab5991
ivan-herrero <ivanhnun@gmail.com>
2019-09-19 11:40:47 +0200

    Add redux and middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
+import 'semantic-ui-css/semantic.min.css'
 
-import App from './App';
+import reducers from './reducers'
+import { recipeSaga } from './sagas/recipe'
+import AppContainer from './containers/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+)
+
+sagaMiddleware.run(recipeSaga)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('root')
+);
